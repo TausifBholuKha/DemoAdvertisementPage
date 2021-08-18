b6b8ad8b78e6bad9a1757582363d4c103b1e98d0
Tausif <tausif@khan.com>
2021-08-18 15:50:47 +0530

    Advertisement and Offer's Website

---
+++ images/js/main.js
@@ -0,0 +1,10 @@
+
+$(".slider")
+.not(".slick-inialized")
+.slick({
+   prevArrow:".site-slider .prev",
+   nextArrow:".site-slider .next",
+   slidesToShow: 4,
+   slidesToScroll: 1,
+   autoplaySpeed:2000
+});
